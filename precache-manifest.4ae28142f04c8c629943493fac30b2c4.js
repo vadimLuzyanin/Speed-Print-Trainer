c5fccc053068120ca443c0e3c61151139c40fc80
vadimLuzyanin <luzianin999@inbox.ru>
2020-04-18 18:49:40 +0300

    Updates

---
+++ precache-manifest.4ae28142f04c8c629943493fac30b2c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de08d67ff591ae385864f5712f46478",
+    "url": "/Speed-Print-Trainer/index.html"
+  },
+  {
+    "revision": "34f9a654c9304d9f4932",
+    "url": "/Speed-Print-Trainer/static/js/2.160ca7b8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Speed-Print-Trainer/static/js/2.160ca7b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9102dd81376306fb1c3",
+    "url": "/Speed-Print-Trainer/static/js/main.fdd1470b.chunk.js"
+  },
+  {
+    "revision": "54d7e7827600767c309b",
+    "url": "/Speed-Print-Trainer/static/js/runtime-main.cbd76a92.js"
+  }
+]);
